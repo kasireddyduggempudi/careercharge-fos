e02227a366a12878ff027449aeee70854bc7cf69
kasi reddy <duggempudi111@gmail.com>
2023-05-23 10:39:50 +0530

    added path

--- src/views/404/index.js
+++ src/views/404/index.js
@@ -1,6 +1,7 @@
-function NotFoundView() {
+function NotFoundView(props) {
     return (
         <div>
+            {JSON.stringify(props.location)}
             <h1 style={{textAlign: "center", padding: "10px"}}>Oops!! Page not found. <a href="/">Career Charge</a></h1>
         </div>
     )
